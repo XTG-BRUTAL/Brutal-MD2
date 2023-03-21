eb6f39b2662fe9ea1cfd5aa0dd7f31574a1872e9
XTG BRUTAL <92993386+XTG-BRUTAL@users.noreply.github.com>
2023-03-22 01:11:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['919101765679'] //ur owner number
+global.ownername = "Brutal" //ur owner name
+global.ytname = "YT: Exe4Us" //ur yt chanel name
+global.socialm = "GitHub: BrutalExe" //ur github or insta name
+global.location = "India" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.ownernomer = "919101765679" //ur number
+global.premium = ['919101765679'] //ur premium number
+global.botname = 'Brutal-MD2' //ur bot name
+global.linkz = "https://chat.whatsapp.com/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/exe4us" //ur website to be displayed
+global.botscript = 'https://github.com/brutalexe' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "Brutal-MD2\n\n\nA whatsapp bot developed by\n🦄BrutalExe\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +919101765679" //ur sticker watermark author
+global.wm = "Brutal Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
